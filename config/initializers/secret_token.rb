141c6a6ccddb7bb482336eb97ffe94836b7240a1
Warren Krewenki <krewenki@gmail.com>
2012-01-22 14:03:38 -0400

    Empty rails app to start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LearnEmberJs::Application.config.secret_token = '956fa39ab08a4f1cb145ee7f38df1f4418cdfc26941487c4da6d31f4b99acc5463e7616a1f9458706283fc051fe8d1a5b15a0985bd21346447e2e694b375a435'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+LearnEmberJs::Application.config.session_store :cookie_store, key: '_Learn-Ember.js_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# LearnEmberJs::Application.config.session_store :active_record_store
